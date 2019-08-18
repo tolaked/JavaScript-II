238556fecd51f4ca6e96136f15aa9b4be3ff69b0
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-18 14:01:27 +0200

    More clarification on closures

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -459,20 +459,20 @@ const runners = [
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
 runners.forEach(name => fullNames.push(`${name.first_name} ${name.last_name}`));
-// console.log(fullNames);
+console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
 runners.forEach(name => firstNamesAllCaps.push(name.first_name.toUpperCase()));
-// console.log(firstNamesAllCaps);
+console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
 const size = runners.filter(runner => runner.shirt_size === "L");
 runnersLargeSizeShirt.push(size);
-// console.log(runnersLargeSizeShirt);
+console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
